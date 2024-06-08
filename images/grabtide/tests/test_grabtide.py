a57237fa9ccdb9e77698209d1130aa40a9e8061b
chester <asuncionmcv@gmail.com>
2024-06-08 14:18:20 +0800

    add test file for grabtide

---
+++ images/grabtide/tests/test_grabtide.py
@@ -0,0 +1,12 @@
+import grabtide
+
+def test_initializeGrabTide(mocker):
+    startDate = 20240101
+    endDate = 20240131
+    saveDir = "path/to/saveDir/"
+
+    args = [startDate, endDate, saveDir]
+    grabTide =  
+    assert isinstance(grabtide.Grabtide)
+
+  
