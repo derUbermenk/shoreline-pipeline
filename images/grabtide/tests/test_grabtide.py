8e95011d31cfd4b3b536dfa6f548c8a8bf9ffb8e
chester <asuncionmcv@gmail.com>
2024-06-08 17:09:45 +0800

    change start and enddate args in test to string

--- images/grabtide/tests/test_grabtide.py
+++ images/grabtide/tests/test_grabtide.py
@@ -1,15 +1,39 @@
-from grabtide.tide_grabber import TideGrabber
+from grabtide import TideGrabber, checkDirExists, initializeTideGrabber
+import tempfile
+
+def test_checkDirExists(mocker):    
+    temp_path = tempfile.TemporaryDirectory()
+    nonExistentFilePath = "/non/existent/path/"
+    existentFilePath = temp_path.name
+
+    # it exits with code 1 if path does not exist
+    try:
+        checkDirExists(nonExistentFilePath)
+    except SystemExit as e:
+        assert True 
+        assert e.code == 1
+    else:
+        assert False 
+
+    # it does not exit if path exist
+    try:
+        checkDirExists(existentFilePath)
+    except SystemExit as e:
+        assert False
+    else:
+        assert True
+
 
 def test_initializeTideGrabber(mocker):
-    startDate = 20240101
-    endDate = 20240131
+    startDate = "20240101"
+    endDate = "20240131"
     saveDir = "path/to/saveDir/"
     expected_savePath = "{saveDir}{startDate_endDate}.csv"
 
     args = [startDate, endDate, saveDir]
 
     # mock checkPath
-    mocker.patch('grabtide.tide_grabber.checkPath')
+    mocker.patch('grabtide.tide_grabber.checkDirExists')
 
     tide_grabber =  initializeTideGrabber(args)
     assert isinstance(tide_grabber, TideGrabber)
