5c3e69bc24e3452d4ceb3bd9e53462abb2d1707b
chester <asuncionmcv@gmail.com>
2024-06-08 16:01:51 +0800

    add test for initializeTideGrabber

--- images/grabtide/tests/test_grabtide.py
+++ images/grabtide/tests/test_grabtide.py
@@ -1,6 +1,6 @@
 from grabtide.tide_grabber import TideGrabber
 
-def test_initializeGrabTide(mocker):
+def test_initializeTideGrabber(mocker):
     startDate = 20240101
     endDate = 20240131
     saveDir = "path/to/saveDir/"
@@ -8,11 +8,12 @@ def test_initializeGrabTide(mocker):
 
     args = [startDate, endDate, saveDir]
 
-    tide_grabber =  initializeTideGrabber()
+    # mock checkPath
+    mocker.patch('grabtide.tide_grabber.checkPath')
+
+    tide_grabber =  initializeTideGrabber(args)
     assert isinstance(tide_grabber, TideGrabber)
 
-    assert formatter.startDate == startDate
-    assert formatter.endDate == endDate
-    assert formatter.save_path == expected_savePath 
-
-  
+    assert tide_grabber.startDate == startDate
+    assert tide_grabber.endDate == endDate
+    assert tide_grabber.save_path == expected_savePath 
