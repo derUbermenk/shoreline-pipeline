fd96b2f8bf9ff2249732706fcaa0260d0cd4e9f5
chester <asuncionmcv@gmail.com>
2024-06-08 16:54:45 +0800

    add code for tide grabber

--- images/grabtide/grabtide/tide_grabber.py
+++ images/grabtide/grabtide/tide_grabber.py
@@ -4,9 +4,33 @@ import argparse
 import requests
 
 class TideGrabber():
-    def __init__():
+    def __init__(self):
         return
 
     def run(self):    
         return
     
+def initializeTideGrabber(args) -> TideGrabber:
+    parser = argparse.ArgumentParser(
+        prog="TideGrabber",
+        description="downloads a csv file of MSL tide data from NOAA websites"
+    )
+
+    parser.add_argument('startdate', help='start date of range (yyyymmdd)')
+    parser.add_argument('enddate', help='end date of range (yyyymmdd)')
+    parser.add_argument('saveDir', help='save dir of csv file')
+    parser.add_argument('--stationid', help='stationid')
+    parser.add_argument('--units', help='units')
+    parser.add_argument('--interval', help='interval')
+    parser.add_argument('--timezone', help='timezone')
+    parser.add_argument('--datum', help='datum')
+
+    args_ = parser.parse_args(args)
+
+    checkSaveDir(args.saveDir)
+
+    tide_grabber = TideGrabber(args.startdate, args.enddate, args.saveDir)
+    return tide_grabber
+
+
+    return 
