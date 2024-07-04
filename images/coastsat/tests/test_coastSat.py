9373bfed6a31e1e010c8838b56c7d5068f27f387
chester <asuncionmcv@gmail.com>
2024-07-04 09:52:21 +0800

    add test for asserting file type and existence

--- images/coastsat/tests/test_coastSat.py
+++ images/coastsat/tests/test_coastSat.py
@@ -1,7 +1,45 @@
-from CoastSat import initializeCoastSatRunner, CoastSatRunner
+from CoastSat import initializeCoastSatRunner, assertfile_type_and_exists, CoastSatRunner
+from unittest.mock import Mock, patch
+
+def test_assertfile_type_and_exists():
+    with patch("os.path.isfile", return_value=True):
+        # assuming file exists and is of correct type
+        existing_file_of_correct_type = "/existing-dir/existing.csv"
+        expected_extension = ".csv"
+        try:
+            assertfile_type_and_exists(existing_file_of_correct_type, expected_extension)
+        except SystemExit as e:
+            assert False 
+        else:
+            assert True 
+        
+        # assuming file exists and but not of correct type
+        existing_file_of_wrong_type = "/existing-dir/existing.csv"
+        expected_extension = ".geojson"
+        try:
+            assertfile_type_and_exists(existing_file_of_wrong_type, expected_extension)
+        except SystemExit as e:
+            assert True
+            code, message= e.code
+            assert code == 1
+            assert message == "/existing-dir/existing.csv has wrong extension. expected .geojson"
+        else:
+            assert False 
+    
+    # fails if file dne 
+    non_existent_file_path = "/absolutely_non_existent_file_path-7018ab22-59e5-499d-a7f1-a1afc35c310f/file.csv"
+    expected_extension = ".csv"
+    try:
+        assertfile_type_and_exists(non_existent_file_path, expected_extension)
+    except SystemExit as e:
+        assert True
+        code, message= e.code
+        assert code == 1
+        assert message == f"cant find file {non_existent_file_path}"
+    else:
+        assert False 
 
 def test_initializeCoastSatRunner():
-
 
     startDate = "2024-01-01"
     endDate = "2024-02-01"
@@ -46,3 +84,5 @@ def test_initializeCoastSatRunner():
     assert coastSatRunner.epsg == epsg
     assert coastSatRunner.path_to_transects == transects
     assert coastSatRunner.path_to_tides == tides
+
+
