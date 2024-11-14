7a2060c57fc496edca5d28fa5daf3250342a13e5
chester <asuncionmcv@gmail.com>
2024-11-14 21:00:25 +0800

    Add terraform config

--- images/grabtide/grabtide_runner.sh
+++ images/grabtide/grabtide_runner.sh
@@ -7,7 +7,6 @@ show_help() {
   echo "  end date"
   echo "  save path"
   echo "  station id"
-  echo "  branch"
   exit 0
 }
 
@@ -19,14 +18,58 @@ startDate=$1
 endDate=$2
 savePath=$3
 station_id=$4
-branch=$5
+bucketName=$5
+s3Key=$6
 
-link=https://api.github.com/repos/derUbermenk/grabtide/tarball/$branch
+shift 6
+
+# Check if the required parameters are provided
+if [[ -z "$startDate" || -z "$endDate" || -z "$savePath" || -z "$station_id" || -z "$bucketName" || -z "$s3Key" ]]; then
+    echo "Error: Missing required parameters!"
+    exit 1
+fi
+
+link=https://api.github.com/repos/derUbermenk/grabtide/tarball/$BRANCH
 
 mkdir /_runner
 mkdir /runner
 
-echo "Downloading from $branch"
+# Loop through remaining arguments to parse optional parameters
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --units)
+            units="$2"
+            shift 2
+            ;;
+        --interval)
+            interval="$2"
+            shift 2
+            ;;
+        --timezone)
+            timzeone="$2"
+            shift 2
+            ;;
+        --datum)
+            datum="$2"
+            shift 2
+            ;;
+        --useAccessKeys)
+            useAccessKeys="true"
+            shift
+            ;;
+        # Catch-all for unknown flags
+        --*) 
+            echo "Unknown option: $1"
+            shift
+            ;;
+        *)
+            echo "Unexpected argument: $1"
+            shift
+            ;;
+    esac
+done
+
+echo "Downloading from $BRANCH"
 echo " using: $link"
 python -m wget -o /runner.tar $link 
 
@@ -37,5 +80,16 @@ chmod +x /runner/tide_grabber
 
 export PATH="/runner:$PATH"
 
-echo "Running tide grabber"
-tide_grabber $startDate $endDate $savePath $station_id 
+echo "Running tide grabber with the following arguments"
+echo "startDate $startDate"
+echo "endDate $endDate"
+echo "savePath $savePath"
+echo "station_id $station_id"
+echo "useAccessKeys $useAccessKeys"
+
+tide_grabber $startDate $endDate $savePath $station_id $bucketName $s3Key\
+ ${units:+--units "$units"}\
+ ${interval:+--interval "$interval"}\
+ ${timezone:+--timezone "$timezone"}\
+ ${datum:+--datum "$datum"}\
+ ${useAccessKeys:+--useAccessKeys}
